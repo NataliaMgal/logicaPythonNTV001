84996926076d6e60d33a0f3800da47409783a54f
Natalia Morales <nataliamgal95@gmail.com>
2022-02-25 20:56:56 -0500

    NTV001 se crea ejemplo de 3 condiciones de la represa de hidroituango

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#comentario
-
-print('Que miedo la guerra')
-
 nivelAgua = int(input("Digita la cantidad de agua de la represa: "))
-print(f"El nivel de agua es {nivelAgua}")
 
-print("Hola :)")
+if(nivelAgua <200):
+    print("No tengo agua")
+elif(nivelAgua >=200 and nivelAgua <450):
+    print("todo bien energia corriente")
+else:
+    print("ojo con caucasia");     
